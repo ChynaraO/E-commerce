7d406e06458420d71c707ee533694cfaed884c49
ChynaraO <omurzakova_chyna@mail.ru>
2026-02-15 22:38:10 -0600

    Add backend as normal folder

---
+++ backend/src/main/java/com/example/SpringEcom/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.example.SpringEcom.controller;
+
+import com.example.SpringEcom.model.Product;
+import com.example.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<> (productService.getAll(), HttpStatus.ACCEPTED);
+    }
+}
