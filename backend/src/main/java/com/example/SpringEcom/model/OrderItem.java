669604558465fe9d8e7595c1abc1831c1e12113a
ChynaraO <omurzakova_chyna@mail.ru>
2026-02-21 20:43:11 -0600

    Place Order in Serice Part 1

--- backend/src/main/java/com/example/SpringEcom/model/OrderItem.java
+++ backend/src/main/java/com/example/SpringEcom/model/OrderItem.java
@@ -17,6 +17,7 @@ import java.math.BigDecimal;
 @Builder
 public class OrderItem {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @ManyToOne
     private Product product;
--- backend/src/main/java/com/example/SpringEcom/service/OrderService.java
+++ backend/src/main/java/com/example/SpringEcom/service/OrderService.java
@@ -1,14 +1,26 @@
 package com.example.SpringEcom.service;
 
+import com.example.SpringEcom.model.Order;
 import com.example.SpringEcom.model.dto.OrderRequest;
 import com.example.SpringEcom.model.dto.OrderResponse;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
-    public OrderResponse placeOrder(OrderRequest orderRequest) {
+    public OrderResponse placeOrder(OrderRequest request) {
+        Order order = new Order();
+        String orderId = "ORD" + UUID.randomUUID().toString().substring(0,8).toUpperCase();
+        order.setOrderId(orderId);
+        order.setCustomerName(request.email());
+        order.setStatus("PLACED");
+        order.setOrderDate(LocalDate.now());
+
+
+
         return null;
     }
 
