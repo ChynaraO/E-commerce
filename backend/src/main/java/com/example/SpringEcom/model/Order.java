26e51e3d0223148f5a3be3a5e33be001d80c47e8
ChynaraO <omurzakova_chyna@mail.ru>
2026-02-21 19:37:40 -0600

    Creating models for orders

---
+++ backend/src/main/java/com/example/SpringEcom/model/Order.java
@@ -0,0 +1,26 @@
+package com.example.SpringEcom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    private Long id;
+    @Column(unique = true)
+    private String orderId;
+    private String customerName;
+    private String status;
+    private LocalDate orderDate;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    List<OrderItem> orderItems;
+}
